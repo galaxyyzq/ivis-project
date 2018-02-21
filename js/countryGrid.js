f762a539b673b5f1b85b946b765e6255d41ae969
muratbuldu <a.muratbuldu@gmail.com>
2018-02-21 20:17:22 +0100

    Make it big again vol1

--- js/countryGrid.js
+++ js/countryGrid.js
@@ -31,8 +31,15 @@ d3.csv("data/1951-data.csv", function(error, data){
       .data(data).enter()
     .append("g")
       .attr("class", "rect-container")
-  	  .attr("id", function(d,i){return "square_"+i});
-	
+  	  .attr("id", function(d,i){return "square_"+i})
+      .on("mouseover", function(d){
+        d3.select(this).attr("transform", "scale(1.2,1.2)");
+      })
+      .on("mouseleave", function(d) {
+        d3.select(this).attr("transform", "scale(1,1)");
+
+      });
+
   // append a sqare svg element in each g container
   square
     .append("rect")
@@ -41,22 +48,22 @@ d3.csv("data/1951-data.csv", function(error, data){
       .attr("stroke-width", 3)
       .attr("stroke", "black")
       .on("mouseover", function(d){
-        var currentX = d3.select(this).attr("x")
-        var currentY = d3.select(this).attr("y")
-        d3.select(this)
-          .attr("x", +currentX - +squareHoverSizeIncrease - +zoomOffset)
-          .attr("y", +currentY - +squareHoverSizeIncrease - +zoomOffset)
-          .attr("width", +squareWidthHeight + +squareHoverSizeIncrease)
-          .attr("height", +squareWidthHeight + +squareHoverSizeIncrease)
+        // var currentX = d3.select(this).attr("x")
+        // var currentY = d3.select(this).attr("y")
+        // d3.select(this)
+        //   .attr("x", +currentX - +squareHoverSizeIncrease - +zoomOffset)
+        //   .attr("y", +currentY - +squareHoverSizeIncrease - +zoomOffset)
+        //   .attr("width", +squareWidthHeight + +squareHoverSizeIncrease)
+        //   .attr("height", +squareWidthHeight + +squareHoverSizeIncrease)
       })
       .on("mouseleave", function(d) {
-        var currentX = d3.select(this).attr("x")
-        var currentY = d3.select(this).attr("y")
-        d3.select(this)
-          .attr("x", +currentX + +squareHoverSizeIncrease + +zoomOffset)
-          .attr("y", +currentY + +squareHoverSizeIncrease + +zoomOffset)
-          .attr("width", squareWidthHeight)
-          .attr("height", squareWidthHeight)
+        // var currentX = d3.select(this).attr("x")
+        // var currentY = d3.select(this).attr("y")
+        // d3.select(this)
+        //   .attr("x", +currentX + +squareHoverSizeIncrease + +zoomOffset)
+        //   .attr("y", +currentY + +squareHoverSizeIncrease + +zoomOffset)
+        //   .attr("width", squareWidthHeight)
+        //   .attr("height", squareWidthHeight)
       })
       .attr("x", function(d,i) {
         return i%numRows * (squareWidthHeight + squareMarginX);
@@ -65,21 +72,21 @@ d3.csv("data/1951-data.csv", function(error, data){
         return Math.floor(i/numRows) * (squareWidthHeight + squareMarginX);
       })
       .attr("fill", "white");
-		
-	
-	square	
-		.each(function(d,i) { 
+
+
+	square
+		.each(function(d,i) {
 			var barHolderSelector = "#"+d3.select(this).attr("id");
 			//console.log(barHolderSelector);
 			var x = +d3.select(this).selectAll("rect").attr("x");
 		    var y = +d3.select(this).selectAll("rect").attr("y");
-			
+
 			console.log("x= "+x + "	y= " + y);
-		
+
 			drawBars(barHolderSelector,xComp="letter",yComp="frequency",yAxisTitle="",height=100,width=100, x, y)
 		});
-  	
-	
+
+
 	/*
 	square.selectAll(".bar")
 		.data(dataChart).enter()
@@ -89,11 +96,8 @@ d3.csv("data/1951-data.csv", function(error, data){
 			.attr("height", 10)
 			.attr("fill", "black");
 	*/
-	
- 
-		
 
-});
 
 
 
+});
