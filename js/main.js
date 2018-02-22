7ef40270cae96de37f02380e8d0dd993c3d44ed3
Staffan Sandberg <staffan.sandberg94@gmail.com>
2018-02-22 21:16:24 +0100

    our data in array by year in main.js

--- js/main.js
+++ js/main.js
@@ -12,5 +12,26 @@ $("#toggleMap").on("click",function(){
 })
 
 
+// Get our current data in a list with each element as our year
+d3.csv("data/data_10years_sorted_country.csv", function(data){
+  var countryWithYears = [];
+  var thisCountry;
+  var prevCountry = data[0];
+  var countryArray = [];
+  data.forEach(function(d,i){
+    // console.log(d)
+    thisCountry = d;
+    if(thisCountry.Country != prevCountry.Country || data[i+1] === undefined){
+      countryWithYears.push(countryArray)
+      countryArray = [];
+    }
+    else{
+      countryArray.push(thisCountry)
+    }
+    prevCountry = thisCountry;
 
-drawBars("#right-side-bar-chart",xComp="letter",yComp="frequency",yAxisTitle="",height=200,width=500, xP=0, yP=0, showAxis=true)
+  })
+  console.log(countryWithYears)
+});
+
+// drawBars("#right-side-bar-chart",xComp="letter",yComp="frequency",yAxisTitle="",height=200,width=500, xP=0, yP=0, showAxis=true)
