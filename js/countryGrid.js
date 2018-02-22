dc10d1b276e3ec105946482b146a09247dc6ef26
kaua-melo <kaua.meloo@gmail.com>
2018-02-22 22:23:38 +0100

    Adding the name of the country

--- js/countryGrid.js
+++ js/countryGrid.js
@@ -1,19 +1,54 @@
-// var dataChart = [10,30,50,20,23,12,32];
-var margin = { top: 60, right: 0, bottom: 10, left: 50 },
-  width = 1200 - margin.left - margin.right,
-  height = 1000 - margin.top - margin.bottom;
 
-var squareWidthHeight = 100;
-var squareMarginX = 10;
-var numRows = 10;
+// Dimensions of the useful area inside the SGV
+
+var margin = { top: 22, right: 20, bottom: 20, left: 20 };
+var width  = 1055; 
+var height = 650;
+
+var squareWidthHeight = 54;
+var squareMarginX = 5;
+//var numRows = Math.floor(width/(squareWidthHeight+squareMarginX) );
+var numRows = 18;
+
 var squareHoverSizeIncrease = 50;
 var zoomOffset = 5;
 
+
 var countryGridSVG = d3.select("#country-grid")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+  //.attr("width", width)
+  //.attr("height", height)
+  .attr("width",  width  + margin.left + margin.right)
+  .attr("height", height + margin.top  + margin.bottom)
 .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+  .attr("id", "g_container"); // We give the <g> an id because we'll later create a rec inside of it
+							  //  that will represent the useful area.
+  
+// Changing SVG background Color
+//$("#country-grid").css('background-color', 'yellow');
+
+// Creating a rect inside the <g> that contains everything so we can fill it with color
+var g_container = d3.select("#g_container")
+	.append("rect")
+    .attr("width",  width+"px")
+    .attr("height", height+"px")
+    .attr("fill", "pink");
+
+// Creating a rect inside the <g> that will draw a cool border
+var a = 20;
+var g_container = d3.select("#g_container")
+	.append("rect")
+	.attr("x", -a/2)
+	.attr("y", -a/2)
+    .attr("width",  width  + a +"px")
+    .attr("height", height + a +"px")
+    .attr("stroke-width", 1) // border
+    //.attr("stroke", "gray")
+    .attr("fill", "lightblue")
+	.attr("rx", 10);
+
+
+
 
 function zoomInSquare(d,i) {
   var currentX = +d3.select(this).select("rect").attr("x"), // Current x position of square in parent
@@ -33,8 +68,10 @@ function zoomOutSquare() {
 
     d3.select(this)
       .attr("transform", `translate(${currentX},${currentY}) scale(1,1) translate(-${currentX},-${currentY})`);
-  }
+}
 
+
+// Reading data and creating the squares
 d3.csv("data/1951-data.csv", function(error, data){
   if(error) throw error;
 
@@ -53,7 +90,7 @@ d3.csv("data/1951-data.csv", function(error, data){
     .append("rect")
       .attr("width", squareWidthHeight)
       .attr("height", squareWidthHeight)
-      .attr("stroke-width", 3)
+      .attr("stroke-width", 1) // border
       .attr("stroke", "black")
       .attr("x", function(d,i) { return i%numRows * (squareWidthHeight + squareMarginX); })
       .attr("y", function(d,i) { return Math.floor(i/numRows) * (squareWidthHeight + squareMarginX); })
