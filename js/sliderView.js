cf548c83d49f1e71d949840e5aacd46a749f5342
Staffan <staffan.sandberg94@gmail.com>
2018-04-05 11:29:50 +0200

    No comma in slider anymore

--- js/sliderView.js
+++ js/sliderView.js
@@ -1,5 +1,5 @@
     // Initialize slider
-    var Slider = d3.slider().min(1951).max(2016).ticks(10).showRange(true).value(2015);
+    var Slider = d3.slider().min(1951).max(2016).ticks(10).showRange(true).value(2015).tickFormat(d3.format("d"));
 
     // Render the slider in the div
     d3.select('#slider').call(Slider);
